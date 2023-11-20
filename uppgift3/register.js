8aba4d5cd7c851fffe09a563d3a5db84e0b9e0d9
necla <123477628+necla87@users.noreply.github.com>
2023-11-20 23:57:52 +0100

    last touch

--- uppgift3/register.js
+++ uppgift3/register.js
@@ -1,24 +1,36 @@
 $(document).ready(function () {
   $('#register-form').submit(function (event) {
-    event.preventDefault(); 
-    var playerName = $('#player-name').val();
-    registerNewPlayer(playerName);
-  });
-});
+    event.preventDefault();
 
-function registerNewPlayer(playerName) {
-  $.ajax({
-    url: 'http://localhost:3000/players', 
-    method: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify({ name: playerName }),
-    success: function (response) {
-      console.log('Registration Success:', response);
-      $('#registration-status').text('Player ' + playerName + ' registered successfully!');
-    },
-    error: function (error) {
-      console.log('Error registering player:', error);
-      $('#registration-status').text('Error registering player. Please try again.');
+    var playerName = $('#player-name').val();
+
+    if (playerName.trim() === '') {
+      displayRegistrationStatus('Please enter a valid player name.');
+      return;
     }
+
+
+    var newPlayer = {
+      name: playerName,
+      gameHistory: [],
+      score: 0
+    };
+
+    $.ajax({
+      url: 'http://localhost:3000/players', 
+      contentType: 'application/json',
+      data: JSON.stringify(newPlayer),
+      success: function (player) {
+        displayRegistrationStatus('Player registered successfully!');
+      },
+      error: function (error) {
+        console.log('Error registering player:', error);
+        displayRegistrationStatus('Error registering player. Please try again.');
+      }
+    });
   });
-}
+
+  function displayRegistrationStatus(message) {
+    $('#registration-status').text(message);
+  }
+});
