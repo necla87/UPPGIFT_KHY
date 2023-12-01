28e96486033e9991eafc927748aea6a94740383d
necla <123477628+necla87@users.noreply.github.com>
2023-12-01 02:38:13 +0100

    done some changes

--- uppgift2_1/index.js
+++ uppgift2_1/index.js
@@ -6,19 +6,16 @@ import EventTicket from './eventTicket.js';
 const usersFile = 'users.json';
 const eventsFile = 'events.json';
 
-let loadedUsers = loadJSON(usersFile) || [];
-let loadedEvents = loadJSON(eventsFile) || [];
-let eventCounter = 1;
-
-function loadJSON(filename) {
+async function loadJSON(filename) {
   try {
-    const data = fs.readFileSync(filename, 'utf8');
+    const data = await fs.readFile(filename, 'utf8');
     return JSON.parse(data);
   } catch (error) {
     console.error(`Error reading ${filename}: ${error.message}`);
     return null;
   }
 }
+
 async function saveJSON(filename, data) {
   try {
     await fs.writeFile(filename, JSON.stringify(data, null, 2), 'utf8');
@@ -28,7 +25,27 @@ async function saveJSON(filename, data) {
   }
 }
 
+let loadedUsers = loadJSON(usersFile) || [];
+if (!Array.isArray(loadedUsers)) {
+  loadedUsers = [];
+}
 
+let loadedEvents = loadJSON(eventsFile) || [];
+if (!Array.isArray(loadedEvents)) {
+  loadedEvents = [];
+}
+
+let eventCounter = 1;
+
+function generateUniqueId(type) {
+  if (type === 'user') {
+    return `user_${loadedUsers.length + 1}`;
+  } else if (type === 'event') {
+    return `event_${eventCounter++}`;
+  } else {
+    throw new Error('Invalid type specified for generateUniqueId');
+  }
+}
 
 function login(username, password) {
   const admin = new Admin('admin_user', 'admin_password');
@@ -46,8 +63,10 @@ function login(username, password) {
   }
 }
 
-function createNewAccount(id, username, password, email, phone) {
-  const newUser = new User(id, username, password, email, phone);
+function createNewAccount(username, password, email, phone, isAdmin = false) {
+  const userId = generateUniqueId('user');
+  const newUser = new User(userId, username, password, email, phone, isAdmin);
+
   loadedUsers.push(newUser);
 
   saveJSON(usersFile, loadedUsers);
@@ -55,19 +74,12 @@ function createNewAccount(id, username, password, email, phone) {
 }
 
 function addEventTicket(eventName, price, eventTime, buyerId) {
-  const eventId = generateUniqueId();
+  const eventId = generateUniqueId('event');
   const newEventTicket = new EventTicket(eventId, eventName, price, eventTime, buyerId);
   loadedEvents.push(newEventTicket);
 
-  eventCounter++;
-
   saveJSON(eventsFile, loadedEvents);
   console.log('Event ticket added successfully!');
-}
-
-
-function generateUniqueId() {
-  return `event_${eventCounter}`;
 }
 
 function viewUsersByEvent() {
@@ -87,33 +99,33 @@ function viewUsersByEvent() {
     }
   });
 }
+
 function getUsersForEvent(buyerId) {
   return loadedUsers.filter((user) => user.username === buyerId);
 }
-
 
 function printUserInfo(user) {
   console.log(`- User ID: ${user.id}, Username: ${user.username}`);
 }
 
-createNewAccount(1, 'Sami', 'dnvkdnf', 'exam@email.com', '123456789');
+// Example usage
+createNewAccount('Sami', 'dnvkdnf', 'exam@email.com', '123456789');
 login('Sami', 'dnvkdnf');
-createNewAccount(2, 'Ahmet', 'kdlvjaivf', 'ahmet@yahaa.com', '555-1234');
-
-createNewAccount(3, 'Merve', 'adnvonadv', 'merve@gmaid.com', '555-5678');
-createNewAccount(4, 'Özgur', 'pfvoakdvu', 'ozgur@mymy.com', '555-9101');
+createNewAccount('Ahmet', 'kdlvjaivf', 'ahmet@yahaa.com', '555-1234');
+createNewAccount('Merve', 'adnvonadv', 'merve@gmaid.com', '555-5678');
+createNewAccount('Özgur', 'pfvoakdvu', 'ozgur@mymy.com', '555-9101');
+login('Sami', 'dnvkdnf');
 
 addEventTicket('Rock Concert', 50, '12/01/2023 19:00', 'Sami');
 addEventTicket('Classical Music Night', 75, '12/10/2023 20:30', 'Ahmet');
 addEventTicket('Comedy Show', 40, '12/05/2023 18:00', 'Merve');
 
-
 viewUsersByEvent();
 
-createNewAccount(5, 'Ayşe', 'fdhsgd', 'ayse@email.com', '555-1122');
-createNewAccount(6, 'Mustafa', 'oiweur', 'mustafa@yahaa.com', '555-3344');
-createNewAccount(7, 'Zeynep', 'sdfgfd', 'zeynep@gmaid.com', '555-5566');
-createNewAccount(8, 'Ali', 'dfgdfg', 'ali@mymy.com', '555-7788');
+createNewAccount('Ayşe', 'fdhsgd', 'ayse@email.com', '555-1122');
+createNewAccount('Mustafa', 'oiweur', 'mustafa@yahaa.com', '555-3344');
+createNewAccount('Zeynep', 'sdfgfd', 'zeynep@gmaid.com', '555-5566');
+createNewAccount('Ali', 'dfgdfg', 'ali@mymy.com', '555-7788');
 
 login('Ayşe', 'fdhsgd');
 
--- uppgift2_1/user.js
+++ uppgift2_1/user.js
@@ -1,12 +1,15 @@
 export default class User {
-  constructor(id, username, password, email, phone) {
+  constructor(id, username, password, email, phone, isAdmin = false, eventTickets = []) {
     this.id = id;
     this.username = username;
     this.password = password;
     this.email = email;
     this.phone = phone;
-    this.eventTickets = [];
+    this.isAdmin = isAdmin;
+    this.eventTickets = eventTickets;
+  }
+
+  addEventTicket(eventTicketId) {
+    this.eventTickets.push(eventTicketId);
   }
 }
-
-
